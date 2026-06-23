3038d6eab1286c87e26155712dc8acd82f129531
Praveen <praveenbegari09@gmail.com>
2026-06-23 23:00:18 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main {
+
+    public static int binarySearch(int[] arr, int target) {
+         
+         int low =0;
+         int high = arr.length-1;
+         while(low<=high){
+             int mid = low + (high-low)/2;
+             
+             if(arr[mid] == target){
+                 return mid;
+             }
+             else if(arr[mid] > target){
+                 high = mid-1;
+             }
+             else{
+                 low = mid+1;
+             }
+         }
+         return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 7, 9, 11, 13};
+
+        int target = 9;
+
+        int ans = binarySearch(arr, target);
+
+        System.out.println(ans);
+    }
+}
